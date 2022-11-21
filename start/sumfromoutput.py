cf0fbbf1aea73b7b179d3aeaa1355a12e598571c
Dexter40404 <118721356+Dexter40404@users.noreply.github.com>
2022-11-21 22:46:55 +0100

    Update sumfromoutput.py

--- start/sumfromoutput.py
+++ start/sumfromoutput.py
@@ -4,3 +4,13 @@ with open("output.txt", "r", encoding='utf-8') as file:
 # I'd love to sum all total words in output.txt file because server kicks me out after +- 1000 (can't blame them) joined urls, but I cant
 #find the right way to isolate numbers from the file and sum them.
 #I thought the code for this would be somewhat like this, I tried a lot of options but still can't isolate the numbers
+
+#edit 
+  #the code below returned only numbers, I still have to figure out how to sum them
+
+with open("output.txt", "r", encoding='utf-8') as file:
+    for line in file:
+        words = line.find("total words")
+        if words == 0:
+            num = line.split(": ")[1]
+            print(num.strip())
