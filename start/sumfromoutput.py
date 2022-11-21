93f49e5657750dab3d89a1f1c857ba8be4eedc0c
Dexter40404 <118721356+Dexter40404@users.noreply.github.com>
2022-11-21 21:46:12 +0100

    Add files via upload

---
+++ start/sumfromoutput.py
@@ -0,0 +1,5 @@
+with open("output.txt", "r", encoding='utf-8') as file:
+    for line in file:
+        pass
+# I'd love to sum all total words in output.txt file because server kicks me out after +- 1000 (can't blame them) joined urls, but I cant
+#find the right way to isolate numbers from the file and sum them.
