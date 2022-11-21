72e1451ff05cf3c876f5d2b6b11313d467ed1146
Dexter40404 <118721356+Dexter40404@users.noreply.github.com>
2022-11-21 23:22:24 +0100

    Update sumfromoutput.py

--- start/sumfromoutput.py
+++ start/sumfromoutput.py
@@ -14,3 +14,27 @@ with open("output.txt", "r", encoding='utf-8') as file:
         if words == 0:
             num = line.split(": ")[1]
             print(num.strip())
+            
+           
+        
+#edit2
+  # i figured it out!
+    with open("output.txt", "r", encoding='utf-8') as file:
+    output = []
+    for line in file:
+        words = line.find("total words")
+        if words == 0:
+            num = line.split(": ")[1]
+            x = num.strip()
+            output.append(x)
+
+
+str(sum(int(num)for num in output))
+
+print(output)
+print("Total is: " + str(sum(int(num)for num in output)) + '.')
+#result
+  #['2', '1', '49']
+  #Total is: 52
+#
+    
